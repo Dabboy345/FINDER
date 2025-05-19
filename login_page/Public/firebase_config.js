51868104677daab360832ce93b785640d662b151
Sushant <159569641+Dabboy345@users.noreply.github.com>
2025-05-19 18:42:23 +0200

    first commit

---
+++ login_page/Public/firebase_config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmS-i8N6sFOB4khvIpX-_fFN3ITebSS0g",
+  authDomain: "finder-ff519.firebaseapp.com",
+  projectId: "finder-ff519",
+  storageBucket: "finder-ff519.firebasestorage.app",
+  messagingSenderId: "989218762868",
+  appId: "1:989218762868:web:7f5160caf34ddccd92e121",
+  measurementId: "G-6GM4SYPVHT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
