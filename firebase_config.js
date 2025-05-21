04c1882f78bf7c7ad192787ffeb3483d4408c3e3
Sushant <159569641+Dabboy345@users.noreply.github.com>
2025-05-21 09:36:38 +0200

    Corrected the firbase configuration

--- firebase_config.js
+++ firebase_config.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getDatabase, ref, set, update, onValue } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBmS-i8N6sFOB4khvIpX-_fFN3ITebSS0g",
@@ -9,7 +11,7 @@ const firebaseConfig = {
   projectId: "finder-ff519",
   storageBucket: "finder-ff519.appspot.com",  // corrected here
   messagingSenderId: "989218762868",
-  atabaseURL: "https://finder-ff519-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://finder-ff519-default-rtdb.europe-west1.firebasedatabase.app/",
   appId: "1:989218762868:web:7f5160caf34ddccd92e121",
   measurementId: "G-6GM4SYPVHT"
 };
@@ -20,4 +22,4 @@ const db = getFirestore(app);
 const database = getDatabase(app);
 
 
-export { app, auth, db };
+export { app, auth, db, database };
